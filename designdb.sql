af49169cd08c54267d95a329aad8400595e583d2
Steven <06dm20@gmail.com>
2025-05-29 10:41:33 -0500

    definicion del MER

--- designdb.sql
+++ designdb.sql
@@ -1,142 +1,146 @@
-ENUM countrys {
-  USA
-  MEX
-  ESP
-  CAN
-  FRA
-  GER
-  ITA
-  BRA
-  ARG
-  CHN
-  JPN
-  IND
-  RUS
-  AUS
-  GBR
-}
+CREATE TYPE "countrys" AS ENUM (
+  'USA',
+  'MEX',
+  'ESP',
+  'CAN',
+  'FRA',
+  'GER',
+  'ITA',
+  'BRA',
+  'ARG',
+  'CHN',
+  'JPN',
+  'IND',
+  'RUS',
+  'AUS',
+  'GBR'
+);
 
-ENUM media_type {
-  Movie
-  Series
-  Documentary
-  ShortFilm
-  MusicVideo
-  Animation
-  Commercial
-  Trailer
-  LivePerformance
-}
+CREATE TYPE "media_types" AS ENUM (
+  'Movie',
+  'Series',
+  'Documentary',
+  'ShortFilm',
+  'MusicVideo',
+  'Animation',
+  'Commercial',
+  'Trailer',
+  'LivePerformance'
+);
 
-ENUM media_genres {
-  Action
-  Adventure
-  Animation
-  Biography
-  Comedy
-  Crime
-  Documentary
-  Drama
-  Family
-  Fantasy
-  Historical
-  Horror
-  Musical
-  Mystery
-  Romance
-  SciFi
-  Sport
-  Thriller
-  War
-  Western
-}
+CREATE TYPE "media_genres" AS ENUM (
+  'Action',
+  'Adventure',
+  'Animation',
+  'Biography',
+  'Comedy',
+  'Crime',
+  'Documentary',
+  'Drama',
+  'Family',
+  'Fantasy',
+  'Historical',
+  'Horror',
+  'Musical',
+  'Mystery',
+  'Romance',
+  'SciFi',
+  'Sport',
+  'Thriller',
+  'War',
+  'Western'
+);
 
-ENUM musical_genres {
-  Pop
-  Rock
-  HipHop
-  Rap
-  Jazz
-  Classical
-  Electronic
-  Country
-  Reggae
-  Blues
-  Metal
-  RnB
-  Latin
-  Folk
-  Indie
-  Soul
-  Punk
-}
+CREATE TYPE "music_genres" AS ENUM (
+  'Pop',
+  'Rock',
+  'HipHop',
+  'Rap',
+  'Jazz',
+  'Classical',
+  'Electronic',
+  'Country',
+  'Reggae',
+  'Blues',
+  'Metal',
+  'RnB',
+  'Latin',
+  'Folk',
+  'Indie',
+  'Soul',
+  'Punk'
+);
 
-ENUM age_restrictions {
-  G
-  PG
-  PG13
-  R
-  NC17
-  UNRATED
-}
+CREATE TYPE "age_restrictions" AS ENUM (
+  'G',
+  'PG',
+  'PG13',
+  'R',
+  'NC17',
+  'UNRATED'
+);
 
-ENUM music_rating {
-  None
-  Explicit
-  Clean
-  ParentalAdvisory
-  Instrumental
-}
+CREATE TYPE "music_ratings" AS ENUM (
+  'None',
+  'Explicit',
+  'Clean',
+  'ParentalAdvisory',
+  'Instrumental'
+);
 
-TABLE user {
-  user_id INTEGER [PK, NOT NULL]
-  first_name VARCHAR(50) [NOT NULL]
-  last_name VARCHAR(50) [NOT NULL]
-  date_of_birth date [NOT NULL]
-  city_of_birth VARCHAR(50) [NOT NULL]
-  city_of_residence VARCHAR(50) [NOT NULL]
-  username VARCHAR(50) [UNIQUE, NOT NULL]
-  email VARCHAR(100) [UNIQUE, NOT NULL]
-  country country [NOT NULL]
-  active BOOL [NOT NULL]
-  created_at TIMESTAMP [NOT NULL]
-}
+CREATE TABLE "user" (
+  "user_id" INTEGER PRIMARY KEY NOT NULL,
+  "first_name" VARCHAR(50) NOT NULL,
+  "last_name" VARCHAR(50) NOT NULL,
+  "date_of_birth" date NOT NULL,
+  "city_of_birth" VARCHAR(50) NOT NULL,
+  "city_of_residence" VARCHAR(50) NOT NULL,
+  "username" VARCHAR(50) UNIQUE NOT NULL,
+  "email" VARCHAR(100) UNIQUE NOT NULL,
+  "country" countrys NOT NULL,
+  "active" BOOL NOT NULL,
+  "created_at" TIMESTAMP NOT NULL
+);
 
-TABLE media {
-  media_id INTEGER [PK, NOT NULL]
-  title VARCHAR(100) [NOT NULL]
-  genre movies_genres [NOT NULL]
-  audiovisual_type media_type [NOT NULL]
-  age_restriction INTEGER [NOT NULL]
-  created_at DATE [NOT NULL]
-  created_in countrys [NOT NULL]
-}
+CREATE TABLE "media" (
+  "media_id" INTEGER PRIMARY KEY NOT NULL,
+  "title" VARCHAR(100) NOT NULL,
+  "genre" media_genres NOT NULL,
+  "audiovisual_type" media_types NOT NULL,
+  "age_restriction" age_restrictions NOT NULL,
+  "created_at" DATE NOT NULL,
+  "created_in" countrys NOT NULL
+);
 
-TABLE song {
-  song_id INTEGER [PK, NOT NULL]
-  title VARCHAR [NOT NULL]
-  song_genre musical_genres [NOT NULL]
-  song_rating music_rating [NOT NULL]
-  created_at DATE [NOT NULL]
-  created_in countrys [NOT NULL]
-}
+CREATE TABLE "song" (
+  "song_id" INTEGER PRIMARY KEY NOT NULL,
+  "title" VARCHAR(100) NOT NULL,
+  "song_genre" music_genres NOT NULL,
+  "song_rating" music_ratings NOT NULL,
+  "created_at" DATE NOT NULL,
+  "created_in" countrys NOT NULL
+);
 
-TABLE favorite_media {
-  user_id INTEGER [NOT NULL, REF: > user.user_id]
-  media_id INTEGER [NOT NULL, REF: > media.media_id]
-  stars DECIMAL(2,1) [NOT NULL]
-  added_at TIMESTAMP [NOT NULL]
-  indexes {
-    (user_id, media_id) [PK]
-  }
-}
+CREATE TABLE "favorite_media" (
+  "user_id" INTEGER NOT NULL,
+  "media_id" INTEGER NOT NULL,
+  "stars" DECIMAL(2,1) NOT NULL,
+  "added_at" TIMESTAMP NOT NULL,
+  PRIMARY KEY ("user_id", "media_id")
+);
 
-TABLE favorite_songs {
-  user_id INTEGER [NOT NULL, REF: > user.user_id]
-  song_id INTEGER [NOT NULL, REF: > song.song_id]
-  stars DECIMAL(2,1) [NOT NULL]
-  added_at TIMESTAMP [NOT NULL]
-  indexes {
-    (user_id, song_id) [PK]
-  }
-}
+CREATE TABLE "favorite_songs" (
+  "user_id" INTEGER NOT NULL,
+  "song_id" INTEGER NOT NULL,
+  "stars" DECIMAL(2,1) NOT NULL,
+  "added_at" TIMESTAMP NOT NULL,
+  PRIMARY KEY ("user_id", "song_id")
+);
+
+ALTER TABLE "favorite_media" ADD FOREIGN KEY ("user_id") REFERENCES "user" ("user_id");
+
+ALTER TABLE "favorite_media" ADD FOREIGN KEY ("media_id") REFERENCES "media" ("media_id");
+
+ALTER TABLE "favorite_songs" ADD FOREIGN KEY ("user_id") REFERENCES "user" ("user_id");
+
+ALTER TABLE "favorite_songs" ADD FOREIGN KEY ("song_id") REFERENCES "song" ("song_id");
