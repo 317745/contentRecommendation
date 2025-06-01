a546a9fa8eff106cba0d8f9c7e657cb84182d259
Steven <06dm20@gmail.com>
2025-05-31 23:45:55 -0500

    Mejora estructura proyecto

--- API/src/app.py
+++
@@ -1,6 +0,0 @@
-#API\src\app.py
-from flask import Flask
-from src.models.connection import closeConnection
-
-app = Flask(__name__)
-app.teardown_appcontext(closeConnection)
--- API/src/controllers/controllers.py
+++
@@ -1,7 +0,0 @@
-#API\src\controllers\controllers.py
-from src.app import app
-from src.models.models import getMedia
-
-@app.route("/")
-def media():
-    return getMedia()
